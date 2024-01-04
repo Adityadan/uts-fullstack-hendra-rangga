61fbf3150bf68540f0b12b22335f8a2862b14bdd
danirama2 <danirama282@gmail.com>
2024-01-04 19:10:37 +0700

    change the mysql database version to version 7

--- fullstack_uts.sql
+++ fullstack_uts.sql
@@ -1,108 +1,141 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2024 at 01:08 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
- Source Server         : PhpMyAdmin
- Source Server Type    : MySQL
- Source Server Version : 80030 (8.0.30)
- Source Host           : localhost:3306
- Source Schema         : fullstack_uts
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 80030 (8.0.30)
- File Encoding         : 65001
 
- Date: 20/12/2023 09:48:05
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `fullstack_uts`
+--
 
--- ----------------------------
--- Table structure for cerita
--- ----------------------------
-DROP TABLE IF EXISTS `cerita`;
-CREATE TABLE `cerita`  (
-  `idcerita` int NOT NULL AUTO_INCREMENT,
-  `judul` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id_user_pembuat_awal` int NOT NULL,
-  PRIMARY KEY (`idcerita`) USING BTREE,
-  INDEX `id_user_pembuat_awal`(`id_user_pembuat_awal` ASC) USING BTREE,
-  CONSTRAINT `cerita_ibfk_1` FOREIGN KEY (`id_user_pembuat_awal`) REFERENCES `user` (`idusers`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of cerita
--- ----------------------------
-INSERT INTO `cerita` VALUES (4, 'shalom', 160718039);
-INSERT INTO `cerita` VALUES (5, 'CEK SOUND', 160718039);
-INSERT INTO `cerita` VALUES (6, 'dami anjaz', 160718039);
-INSERT INTO `cerita` VALUES (7, 'kemerdekaan', 111);
-INSERT INTO `cerita` VALUES (8, 'tes cerita baru', 160718039);
-INSERT INTO `cerita` VALUES (9, 'kos tenggilis', 160718039);
-INSERT INTO `cerita` VALUES (10, 'tes cerita 4', 160718039);
-INSERT INTO `cerita` VALUES (11, 'tes cerita 5', 160718039);
+--
+-- Table structure for table `cerita`
+--
 
--- ----------------------------
--- Table structure for paragraf
--- ----------------------------
-DROP TABLE IF EXISTS `paragraf`;
-CREATE TABLE `paragraf`  (
-  `idparagraf` int NOT NULL AUTO_INCREMENT,
-  `iduser` int NOT NULL,
-  `idcerita` int NOT NULL,
-  `isiparagraf` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `tgl_buat` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idparagraf`) USING BTREE,
-  INDEX `iduser`(`iduser` ASC) USING BTREE,
-  INDEX `idcerita`(`idcerita` ASC) USING BTREE,
-  CONSTRAINT `paragraf_ibfk_2` FOREIGN KEY (`idcerita`) REFERENCES `cerita` (`idcerita`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `paragraf_ibfk_3` FOREIGN KEY (`iduser`) REFERENCES `user` (`idusers`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `cerita` (
+  `idcerita` int(11) NOT NULL,
+  `judul` varchar(1000) NOT NULL,
+  `id_user_pembuat_awal` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of paragraf
--- ----------------------------
-INSERT INTO `paragraf` VALUES (4, 160718039, 4, 'omswastiastu', '2023-11-21 09:09:22');
-INSERT INTO `paragraf` VALUES (5, 160718039, 4, 'A free Bootstrap 4 admin theme built with HTML/CSS and a modern development workflow environment ready to use to build your next dashboard or web application\r\n\r\n', '2023-11-21 09:09:22');
-INSERT INTO `paragraf` VALUES (10, 160718039, 4, 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.', '2023-11-21 07:41:43');
-INSERT INTO `paragraf` VALUES (11, 160718039, 4, 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.\r\n\r\n', '2023-11-21 07:43:57');
-INSERT INTO `paragraf` VALUES (12, 160718039, 4, 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.\r\n\r\n', '2023-11-21 07:45:54');
-INSERT INTO `paragraf` VALUES (13, 160718039, 4, 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.\r\n\r\n', '2023-11-21 07:47:22');
-INSERT INTO `paragraf` VALUES (14, 160718039, 4, 'Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant\r\n\r\n', '2023-11-21 07:47:37');
-INSERT INTO `paragraf` VALUES (15, 160718039, 4, 'ASD', '2023-11-23 02:01:50');
-INSERT INTO `paragraf` VALUES (16, 160718039, 5, 'AHAY', '2023-11-23 02:02:02');
-INSERT INTO `paragraf` VALUES (17, 160718039, 5, 'BABI\r\n', '2023-11-23 02:02:09');
-INSERT INTO `paragraf` VALUES (18, 160718039, 4, 'ajaoisdaisojdoaisjdjasoid', '2023-12-14 13:30:08');
-INSERT INTO `paragraf` VALUES (19, 160718039, 6, 'tes dami\r\n', '2023-12-14 13:30:23');
-INSERT INTO `paragraf` VALUES (20, 160718039, 6, 'dwi barbar', '2023-12-14 13:30:30');
-INSERT INTO `paragraf` VALUES (21, 160718039, 6, 'iqbal chobir', '2023-12-14 13:30:36');
-INSERT INTO `paragraf` VALUES (22, 111, 7, 'pada tahun 2012', '2023-12-16 14:50:02');
-INSERT INTO `paragraf` VALUES (23, 160718039, 8, 'pada zaman dlu kala hidup hendra dami yang suka membuat onar di dompu', '2023-12-19 07:57:39');
-INSERT INTO `paragraf` VALUES (24, 160718039, 9, 'didekat ubaya terdapat sebuah kos brutal ', '2023-12-19 07:58:56');
-INSERT INTO `paragraf` VALUES (25, 160718039, 9, 'anjaz brutal', '2023-12-19 07:59:09');
-INSERT INTO `paragraf` VALUES (26, 160718039, 7, 'ada seorang mahasiswa yang bernama iqbal chobir bujafar', '2023-12-19 13:22:00');
-INSERT INTO `paragraf` VALUES (27, 160718039, 7, 'iqbal terkenal  sangat brutal', '2023-12-19 13:22:09');
-INSERT INTO `paragraf` VALUES (28, 160718039, 10, 'tes cerita 4tes cerita 4tes cerita 4tes cerita 4tes cerita 4tes cerita 4tes cerita 4tes cerita 4', '2023-12-19 14:58:18');
-INSERT INTO `paragraf` VALUES (29, 160718039, 11, 'tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5tes cerita 5', '2023-12-19 14:58:35');
+--
+-- Dumping data for table `cerita`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `idusers` int NOT NULL,
-  `nama` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `salt` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`idusers`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 160718046 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+INSERT INTO `cerita` (`idcerita`, `judul`, `id_user_pembuat_awal`) VALUES
+(1, 'cerita 1 dari user1', 1234),
+(2, 'cerita 2 dari user2', 5678),
+(3, 'testing judul lagi', 1234);
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (111, 'chobir', 'bc2656bbdfead7a4dcae50334ceafb65ad22e065', '86cb50920f');
-INSERT INTO `user` VALUES (160418002, 'Dwi Rangga Kusuma', 'a286dea5c004dbddd0f36b713242da2a51e91807', '9e17d78e54');
-INSERT INTO `user` VALUES (160718038, 'Hendra Dami', '209d5fae8b2ba427d30650dd0250942af944a0c9', '704277848b');
-INSERT INTO `user` VALUES (160718039, 'dani', '1c191e80eecbf9a41a54b1c287f7003b1c9b3efa', 'e9b612031d');
+-- --------------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Table structure for table `paragraf`
+--
+
+CREATE TABLE `paragraf` (
+  `idparagraf` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idcerita` int(11) NOT NULL,
+  `isiparagraf` text NOT NULL,
+  `tgl_buat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paragraf`
+--
+
+INSERT INTO `paragraf` (`idparagraf`, `iduser`, `idcerita`, `isiparagraf`, `tgl_buat`) VALUES
+(1, 1234, 1, 'cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1', '2023-12-20 07:51:48'),
+(2, 1234, 1, 'cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1\r\n\r\ncerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1\r\n\r\ncerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1\r\n\r\ncerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1cerita 1 dari user1\r\n\r\n', '2023-12-20 07:52:07'),
+(3, 5678, 2, 'cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2cerita 2 dari user2', '2023-12-20 07:53:20'),
+(4, 1234, 3, 'ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ini paragraf ', '2023-12-22 15:24:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `idusers` int(11) NOT NULL,
+  `nama` varchar(1000) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `salt` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`idusers`, `nama`, `password`, `salt`) VALUES
+(1234, 'user1', 'bc15375d3a42e2b2a8cde3797a540fa94dbcb581', '5ae0535260'),
+(5678, 'user2', '607edfd46935df77c38127255529ddea21acfd4d', 'c23b468ca1'),
+(1, 'rangga', '1a51673fe8863959446113fe6ee035e80125152e', 'c36d19b475');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cerita`
+--
+ALTER TABLE `cerita`
+  ADD PRIMARY KEY (`idcerita`),
+  ADD KEY `fk_cerita_id_user_pembuat_awal` (`id_user_pembuat_awal`) USING BTREE;
+
+--
+-- Indexes for table `paragraf`
+--
+ALTER TABLE `paragraf`
+  ADD PRIMARY KEY (`idparagraf`),
+  ADD KEY `fk_paragraf_iduser` (`iduser`),
+  ADD KEY `fk_paragraf_idcerita` (`idcerita`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cerita`
+--
+ALTER TABLE `cerita`
+  MODIFY `idcerita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `paragraf`
+--
+ALTER TABLE `paragraf`
+  MODIFY `idparagraf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idusers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5679;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
